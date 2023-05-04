b120a52c6801c6b4e8ea9840e1c8e40b3b68b084
Yauheny Vial <mr.jk1629003@gmail.com>
2023-05-04 20:07:23 +0300

    feat: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // src: "/src"
+            components:'/src/components',
+            hooks:'/src/hooks',
+            services:'/src/services',
+            assets:'/src/assets',
+            types:'/src/types'
+        }
+    }
 })
